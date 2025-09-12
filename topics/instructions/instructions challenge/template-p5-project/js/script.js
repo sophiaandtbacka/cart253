d98813b286270ff7dbc3da38d6dd0f9f789d3d62
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2025-09-11 22:48:54 -0400

    set up basic background and canvas for project

---
+++ topics/instructions/instructions challenge/template-p5-project/js/script.js
@@ -0,0 +1,37 @@
+/**
+ * Java Jaws
+ * Sophia Andtbacka
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(400, 600)
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background("white");
+
+    //frame
+    noFill();
+    rect(0, 0, 400, 600);
+
+    //sea
+    push();
+    fill(0, 0, 255);
+    rect(0, 200, 400, 400);
+    pop();
+
+
+
+}
