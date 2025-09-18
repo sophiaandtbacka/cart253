bf4a78466ce84a38b2daf7da3a6288c04b810acc
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2025-09-18 12:23:00 -0400

    Finishes introducing variables exercise

--- topics/variables/introducing variables/template-p5-project/js/script.js
+++ topics/variables/introducing variables/template-p5-project/js/script.js
@@ -1,24 +1,29 @@
 /**
- * Title of Project
- * Author Name
+ * Introducing Variables
+ * Sophia Andtbacka
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
-
+ * Learned what a variable is and does
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create a canvas
 */
 function setup() {
+    createCanvas(640, 480)
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a circle in center
 */
 function draw() {
+    background(0);
 
+    //Draw circle
+    push();
+    fill(mouseX, mouseY, 0);
+    noStroke();
+    ellipse(width / 2, height / 2, 100, 100);
+    pop();
 }
