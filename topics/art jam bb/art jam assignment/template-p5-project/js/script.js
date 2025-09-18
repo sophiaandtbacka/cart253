57327fe3250ce433ba60d67d9f71506aadb5d44c
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2025-09-18 16:53:24 -0400

    uploaded art jam assignment template

---
+++ topics/art jam bb/art jam assignment/template-p5-project/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Art Jam: the multifacetted image of Sophia Maryse "Diamond" Andtbacka
+ * Sophia Andtbacka
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
