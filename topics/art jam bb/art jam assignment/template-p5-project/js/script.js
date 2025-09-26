4f6b0c53558900db35b9e8c44a022a97460d7a0a
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2025-09-25 20:23:52 -0400

    top and bottom abstraction level 1 facets

--- topics/art jam bb/art jam assignment/template-p5-project/js/script.js
+++ topics/art jam bb/art jam assignment/template-p5-project/js/script.js
@@ -22,6 +22,21 @@ function setup() {
 */
 function draw() {
     background("#aaaaaa");
+    //outline
+    //top tri
     triangle(150, 500, 300, 100, 450, 500);
+    //bottom tri
     triangle(150, 500, 300, 650, 450, 500);
+    //top row inner
+    triangle(150, 500, 300, 100, 225, 500);
+    triangle(225, 500, 300, 100, 300, 500);
+    triangle(300, 500, 300, 100, 375, 500);
+    triangle(375, 500, 300, 100, 450, 500);
+    //bottom row inner
+    triangle(150, 500, 300, 650, 225, 500);
+    triangle(225, 500, 300, 650, 300, 500);
+    triangle(300, 500, 300, 650, 375, 500);
+    triangle(375, 500, 300, 650, 450, 500);
+
+
 }
