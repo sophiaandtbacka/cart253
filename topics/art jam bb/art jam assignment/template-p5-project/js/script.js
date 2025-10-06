16f8fb8490dab27851cc0657cd8803fbb404c3d1
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2025-10-06 16:46:14 -0400

    wrote code for interaction with buttons, buttons aren't working

--- topics/art jam bb/art jam assignment/template-p5-project/js/script.js
+++ topics/art jam bb/art jam assignment/template-p5-project/js/script.js
@@ -8,11 +8,9 @@
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 
-//cavas website size
+
+//cavas website size constants that I have named stage
 const stage = {
     x: 600,
     y: 750
@@ -22,6 +20,7 @@ const stage = {
 //setup
 function setup() {
     createCanvas(stage.x, stage.y);
+    background("#aaaaaa");
 }
 
 
@@ -30,32 +29,38 @@ function setup() {
  * Draws Gem
 */
 function draw() {
-    background("#aaaaaa");
 
-    //not working rn
+    //constants for split screen
+    //one has hex value the other rgb
     const darkl = {
-        fill: (0, 0, 0)
+        fill: "#000000"
     }
 
-    const leftr = {
-        fill: (255, 255, 255)
+    const lightr = {
+        fill: {
+            r: 255,
+            g: 255,
+            b: 255,
+        }
     }
 
     //black left, white right 
     function splitScreen() {
-        fill(0, 0, 0);
+        push();
+        fill(darkl.fill);
         rect(0, 0, stage.x / 2, stage.y);
-        fill(255, 255, 255);
+        pop();
+
+        push();
+        fill(lightr.fill.r, lightr.fill.g, lightr.fill.b);
         rect(stage.x / 2, 0, stage.x / 2, stage.y);
+        pop();
     }
 
     splitScreen()
 
 
-    //outline
-
-    fill("#ffffff");
-
+    //gem grid constants
     //line a
     const pointa = {
         x: 300,
@@ -180,7 +185,15 @@ function draw() {
     //triangle(300, 500, 300, (500 + 50), (300 + 37.5), 500);
     //triangle((300 + 37.5), 500, 375 - (), (500 + 50), (300 + 37.5), 500);
 
-    //top row triangles 2nd layer abstraction, left to right
+    fill("#ffffff");
+
+    //quad top
+    quad(pointb.b1.x, pointb.b1.y, pointc.c2.x, pointc.c2.y, pointb.b2.x, pointb.b2.y, pointa.x, pointa.y);
+    quad(pointb.b2.x, pointb.b2.y, pointc.c4.x, pointc.c4.y, pointb.b3.x, pointb.b3.y, pointa.x, pointa.y);
+    quad(pointb.b3.x, pointb.b3.y, pointc.c6.x, pointc.c6.y, pointb.b4.x, pointb.b4.y, pointa.x, pointa.y);
+    quad(pointb.b4.x, pointb.b4.y, pointc.c8.x, pointc.c8.y, pointb.b5.x, pointb.b5.y, pointa.x, pointa.y);
+
+    //top row triangles, left to right
     triangle(pointc.c1.x, pointc.c1.y, pointb.b1.x, pointb.b1.y, pointc.c2.x, pointc.c2.y);
     triangle(pointc.c2.x, pointc.c2.y, pointb.b2.x, pointb.b2.y, pointc.c3.x, pointc.c3.y);
     triangle(pointc.c3.x, pointc.c3.y, pointb.b2.x, pointb.b2.y, pointc.c4.x, pointc.c4.y);
@@ -190,15 +203,7 @@ function draw() {
     triangle(pointc.c7.x, pointc.c7.y, pointb.b4.x, pointb.b4.y, pointc.c8.x, pointc.c8.y);
     triangle(pointc.c8.x, pointc.c8.y, pointb.b5.x, pointb.b5.y, pointc.c9.x, pointc.c9.y);
 
-    //quad top
-    quad(pointb.b1.x, pointb.b1.y, pointc.c2.x, pointc.c2.y, pointb.b2.x, pointb.b2.y, pointa.x, pointa.y);
-    //drawQuad1();
-
-    quad(pointb.b2.x, pointb.b2.y, pointc.c4.x, pointc.c4.y, pointb.b3.x, pointb.b3.y, pointa.x, pointa.y);
-    quad(pointb.b3.x, pointb.b3.y, pointc.c6.x, pointc.c6.y, pointb.b4.x, pointb.b4.y, pointa.x, pointa.y);
-    quad(pointb.b4.x, pointb.b4.y, pointc.c8.x, pointc.c8.y, pointb.b5.x, pointb.b5.y, pointa.x, pointa.y);
-
-    //bottom row triangles 2nd layer abstraction, left to right
+    //bottom row triangles and quad, left to right
     triangle(pointc.c1.x, pointc.c1.y, pointc.c2.x, pointc.c2.y, pointf.x, pointf.y,);
     quad(pointc.c2.x, pointc.c2.y, pointc.c3.x, pointc.c3.y, pointe.x, pointe.y, pointf.x, pointf.y);
     quad(pointc.c3.x, pointc.c3.y, pointc.c4.x, pointc.c4.y, pointd.x, pointd.y, pointe.x, pointe.y);
@@ -210,47 +215,77 @@ function draw() {
     triangle(pointc.c8.x, pointc.c8.y, pointc.c9.x, pointc.c9.y, pointf.x, pointf.y,);
 
 
-    //button
+    //buttons
+    //left button 'D' (for depression) constants
     const button1 = {
+        //position and size
         x: 100,
         y: 100,
         size: 50,
-        fill: (255),
-        pressed: (255, 0, 0),
+
+        //colors
+        fill: "#ffffff",
+        fills: {
+            unpressed: "#ffffff",
+            pressed: "#cccccc"
+        },
+
+        //will be loaded in preload
+        soundEffect: undefined
     }
 
+    //right button 'M' (for mania) constants
     const button2 = {
         x: 500,
         y: 100,
         size: 50,
-        fill: 255
-    }
-    function button() {
-        strokeWeight(2);
 
-        fill(255, 255, 255);
-        ellipse(button1.x, button1.y, button1.size);
-
-        textAlign(CENTER);
-        textSize(32);
-        fill(0);
-        textFont('Arial');
-        text('D', button1.x, button1.y + 10);
-
-
-
-
-        fill(0, 0, 0);
-        ellipse(button2.x, button2.y, button2.size);
-
-        textAlign(RIGHT);
-        textSize(32);
-        fill(255);
-        textFont('Arial');
-        text('M', button2.x + 12, button2.y + 11.5);
+        //colors
+        fill: "#000000",
+        fills: {
+            unpressed: "#000000",
+            pressed: "#aaaaaa"
+        },
     }
 
-    button();
+    //load the sound effect
+    function preload() {
+        button1.soundEffect = loadSound("assets/sounds/bark.wav");
+    }
+
+
+    //buttons
+    //might change to no stroke depending on whether I change split screen colors
+    //button 1
+    push();
+    strokeWeight(2);
+    fill(button1.fill);
+    ellipse(button1.x, button1.y, button1.size);
+
+    //text in button 1
+    textAlign(CENTER);
+    textSize(32);
+    fill(0);
+    textFont('Arial');
+    text('D', button1.x, button1.y + 10);
+    pop();
+
+    //button 2
+    push();
+    strokeWeight(2);
+    fill(button2.fill);
+    ellipse(button2.x, button2.y, button2.size);
+
+    //text in button 2
+    textAlign(RIGHT);
+    textSize(32);
+    fill(255);
+    textFont('Arial');
+    text('M', button2.x + 12, button2.y + 11.5);
+    pop();
+
+
+
 
     //colours not working
     //changes when touches pills, not working right now
@@ -260,11 +295,17 @@ function draw() {
         const d = dist(mouseX, mouseY, button1.x, button1.y);
         const overlap = (d < button1.size / 2);
         if (overlap) {
-            button.bark.wav.play();
-            button.fill = button.pressed;
+            button1.soundEffect.play();
+            button1.fill = button1.fills.pressed;
         }
-    };
+        console.log();
 
-    mousePressed();
+    }
+
+
 
 }
+
+function mouseReleased() {
+    button1.fill = button1.fills.unpressed;
+};
