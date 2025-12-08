6a1a8025e71fc6310e788abcb91e975936b92a81
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2025-12-08 04:00:27 -0500

    Pt 3 Done

--- topics/variation jam/js/script.js
+++ topics/variation jam/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Variation Jam
+ * Sophia Andtbacka
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
