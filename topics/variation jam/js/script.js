6013fd93aa917bd61a39fe1c0590d6b81d410caa
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2025-12-10 12:25:52 -0500

    Variation 1 got the wrote most of script, got color change to work but moving not working
    Generating a new circle instead of moving clicked circle

--- topics/variation jam/js/script.js
+++ topics/variation jam/js/script.js
@@ -8,65 +8,69 @@
 
 "use strict";
 
+//I don't think I'll use this in my pseudocode you can see I had a different approach plan
 const circle = {
-    x: random(x, x),
-    y: random(y, y),
+    /*x: random(1, 400),
+    y: random(1, 400),
     colorNoOverlap: {
-        r: random(n, n),
-        g: random(n, n),
-        b: random(n, n),
+        r: random(0, 255),
+        g: random(0, 255),
+        b: random(0, 255),
     },
     colorOverlap: {
-        r: random(n, n),
-        g: random(n, n),
-        b: random(n, n),
+        r: random(0, 255),
+        g: random(0, 255),
+        b: random(0, 255),
     },
-    size: n
+    */
+    size: 40
 };
 
 let circles = [];
-let numCircles = n;
+let numCircles = 40;
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(500, 500);
+
+    for (let i = 0; i < numCircles; i++) {
+        circles.push({
+            x: random(width),
+            y: random(height),
+            size: 40,
+            color: {
+                r: random(100, 200),
+                g: random(100, 100),
+                b: random(100, 200),
+                //a: random(0.9, 1),
+            }
+        });
+    }
 }
 
 function draw() {
-    drawCircle();
-    checkOverlap();
-    mousePressed();
-}
-
-function drawCircle() {
-    // create starting circles
-    for (let i = 0; i < numCircles; i++) {
-        fill(colorNoOverlap);
-        ellipse(x, y, width, height);
-    };
-}
-
-
-function moveCircle(circle) {
-    // fill with the circle's overlap color
-    fill(circle.colorOverlap.r, circle.colorOverlap.g, circle.colorOverlap.b);
-
-    // teleport to a new random position
-    circle.x = random(n, n);
-    circle.y = random(n, n);
-}
-
-function checkOverlap() {
-    const d = distance(mouseX, mouseY, circles.x, circles.y);
-    if (d < circle.size / 2) {
-        checkOverlap = true
-    }
-    else {
-        checkOverlap = false
+    for (let c of circles) {
+        noStroke();
+        fill(c.color.r, c.color.g, c.color.b);
+        ellipse(c.x, c.y, c.size);
     }
 }
 
 function mousePressed() {
-    if (checkOverlap === true) {
-        moveCircle();
+    for (let c of circles) {
+        const d = dist(mouseX, mouseY, c.x, c.y);
+        if (d < c.size / 2) {
+            moveCircle(c);
+        }
     }
+}
+
+function moveCircle(c) {
+    c.x = random(width);
+    c.y = random(height);
+    c.color = {
+        r: random(0, 200),
+        g: random(0, 200),
+        b: random(0, 200),
+        //a: random(0.9, 1),
+    };
 }
