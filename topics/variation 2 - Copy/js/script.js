83f665e352870b640d67a12d518b73a001f0a839
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2025-12-13 20:02:06 -0600

    I might use this for another variation
    Made a mistake in coding my second variation and it turned out cool.

---
+++ topics/variation 2 - Copy/js/script.js
@@ -0,0 +1,60 @@
+/**
+ * Variation Jam
+ * Sophia Andtbacka
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+//NOTES: java loads first everything outside of functions i.e. variables and then loads functions top to bottom
+
+"use strict";
+
+let circle;//creates global variable of circle
+
+
+function setup() {
+    createCanvas(500, 500);
+
+    //assigns values to global variable, great solution if your variable is using p5 functions, otherwise doesn't work because p5 initializes in setup
+    circle = {
+        x: width / 2,
+        y: height / 2,
+        colorStart: {
+            r: random(0, 255),
+            g: random(0, 255),
+            b: random(0, 255),
+            a: random(1, 1)
+        },
+        size: 25
+    };
+}
+
+function draw() {
+    background(255);
+    drawCircle();
+    moveCircle();
+    //colorChange();
+}
+
+function drawCircle() {
+    noStroke();
+    fill(circle.colorStart.r, circle.colorStart.g, circle.colorStart.b, circle.colorStart.a);
+    ellipse(circle.x, circle.y, circle.size);
+}
+
+function moveCircle() {
+    circle.x = mouseX
+    circle.y = mouseY
+}
+
+function colorChange() {
+    if (mouseX !== pmouseX) {
+        circle.colorStart = {
+            r: random(0, 255),
+            g: random(0, 255),
+            b: random(0, 255),
+            a: random(0.8, 1)
+        };
+    }
+}
