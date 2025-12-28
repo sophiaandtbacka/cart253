d1d4afa5fc900217ff26c9bfa7c223f4e6945b1a
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2025-12-28 02:41:13 -0700

    Added game 2 functions

--- topics/my variation menu/template-p5-project/js/script.js
+++ topics/my variation menu/template-p5-project/js/script.js
@@ -46,6 +46,13 @@ let currentY;
 
 /**Game 2 Variables */
 
+//input box variables
+let red;
+let green;
+let blue;
+
+//creates global variable of circle
+let circle;
 
 /**Game 3 Variables */
 
@@ -73,13 +80,6 @@ let colorAmax = 255;
 let circleNumber = 1000;
 
 
-//initial game1 state
-let title = true;
-let data = false;
-let game = false;
-//initial enter button state
-let enter = false;
-
 //array with all circles
 let circles = [];
 
@@ -95,6 +95,14 @@ game3 = false;
 
 //initially you will be on menu screen
 let state = "menu";
+
+//initial individual game states
+let title = true;
+let data = false;
+let game = false;
+
+//initial enter button state
+let enter = false;
 
 //variable for enter button
 const button = {
@@ -119,6 +127,7 @@ let myFont;
 */
 function setup() {
     createCanvas(500, 500);
+    textFont(myFont); //font for whole game
 
     switch (state) {
         case "menu":
@@ -307,48 +316,20 @@ function menuText() {
 
 /**Universal Game functions */
 
-/**Game 3 functions */
-
-function game3Setup() {
-    dataInputs();  //all inputs from data page
-    hideInputs();  //hides all the inputs
-}
-
-function game3Draw() {
-    background(255);
-
-    if (title === true) {
-        background(0);
-        hideInputs();
-        titleScreenText();
-        enterButton();
-        checkOverlap();
-    }
-
-    else if (data === true) {
-        background(0);
-        showInputs();
-        dataScreenText();
-        enterButton();
-        checkOverlap();
-    }
-
-    else if (game === true) {
-        background(255);
-        hideInputs();
-        returnData();
-        //draws all of the circles
-        for (let c of circles) {
-            noStroke();
-            fill(c.color.r, c.color.g, c.color.b, c.color.a);
-            ellipse(c.x, c.y, c.size);
-        }
-    }
-}
-
 
 //event that triggers game screen from title screen and moves circle on game screen
 function mouseClicked() {
+    if (state === "color cache variation") {
+        if (enter === true && title === true) {
+            title = false;
+            data = true;
+        }
+        else if (enter === true && data === true) {
+            data = false;
+            game = true;
+        }
+    }
+
     if (state === "bubble buster variation") {
         if (enter === true && title === true) {
             data = true;
@@ -372,21 +353,8 @@ function mouseClicked() {
         }
     }
 
-}
 
-//all title screen text
-function titleScreenText() {
-    fill(255);
-    textAlign(CENTER, CENTER);
-    textFont(myFont);
-    textSize(30);
-    text('BUBBLE BUSTER', width / 2, 100);
 
-    //explanation
-    push();
-    textSize(20);
-    text('XXX', width / 2, 150);
-    pop();
 }
 
 //draws the enter button on the title and data screen
@@ -407,6 +375,241 @@ function checkOverlap() {
     const d = dist(mouseX, mouseY, button.x, button.y);
     enter = d < button.width / 2
 }
+
+
+/**Game 2 functions */
+
+function game2Setup() {
+    dataInputs2(); //input boxes for rgb background value
+    hideInputs2(); //hides all input boxes
+
+    //assigns values to global variable, great solution if your variable is using p5 functions, otherwise doesn't work because p5 initializes in setup
+    circle = {
+        x: width / 2,
+        y: height / 2,
+        colorStart: {
+            r: random(0, 255),
+            g: random(0, 255),
+            b: random(0, 255),
+            a: random(10, 255),
+        },
+        size: 100
+    };
+}
+
+function game2Draw() {
+    if (title === true) {
+        background(0);
+        titleScreenText2();
+        enterButton();
+        checkOverlap();
+    }
+    else if (data === true) {
+        background(0);
+        cursor(ARROW);//include this so if go back to data the cursor will change to default
+        dataScreenText()
+        showInputs2();
+        enterButton();
+        checkOverlap();
+    }
+    else if (game === true) {
+        background(Number(red.value()), Number(green.value()), Number(blue.value()));//input valuse from data page
+        cursor(CROSS);
+        hideInputs2();
+
+        //circle
+        drawCircle();
+        moveCircle();
+        colorChange();
+
+        //text
+        //white padding at bottom so rgba values are always visible
+        push();
+        fill(255);
+        rect(0, 425, width, height);
+        pop();
+        showText();//rgba values
+
+        returnData();//when push enter button on game screen return to data page
+    }
+}
+
+//all title screen text
+function titleScreenText2() {
+    fill(255);
+    textAlign(CENTER, CENTER);
+    textSize(30);
+    text('COLOR CIRCLE', width / 2, 100);
+
+    //explanation
+    push();
+    textSize(20);
+    text('XXX', width / 2, 150);
+    pop();
+}
+
+//all data screen text
+
+function dataScreenText() {
+    fill(255);
+    textAlign(CENTER);
+
+    push();
+    textSize(30);
+    text('CIRCLE DATA', width / 2, 100);
+    pop();
+
+    push();
+    textSize(20);
+    text('RED', 125, 180);
+    text('GREEN', 125, 280);
+    text('BLUE', 125, 380);
+    pop();
+}
+
+//all data input boxes
+function dataInputs2() {
+    redInput();
+    greenInput();
+    blueInput();
+}
+//red min input box
+function redInput() {
+    red = createInput(255);
+    red.size(200);
+
+    red.position(
+        (windowWidth / 2 - 95 + 40),
+        (windowHeight / 2 - 85));
+
+};
+//green min input box
+function greenInput() {
+    green = createInput(255);
+    green.size(200);
+
+    green.position(
+        (windowWidth / 2 - 95 + 40),
+        (windowHeight / 2 + 15));
+
+};
+//blue min input box
+function blueInput() {
+    blue = createInput(255);
+    blue.size(200);
+
+    blue.position(
+        (windowWidth / 2 - 95 + 40),
+        (windowHeight / 2 + 115));
+
+};
+//hides all inputs
+function hideInputs2() {
+    red.hide();
+    green.hide();
+    blue.hide();
+}
+//shows all inputs
+function showInputs2() {
+    red.show();
+    green.show();
+    blue.show();
+}
+
+//draws the circle
+function drawCircle() {
+    noStroke();
+    fill(circle.colorStart.r, circle.colorStart.g, circle.colorStart.b, circle.colorStart.a);
+    ellipse(circle.x, circle.y, circle.size);
+}
+//circle moves based on mouse position, has constraints which created visual padding
+function moveCircle() {
+
+    circle.x = mouseX;
+    circle.y = mouseY;
+
+    circle.x = constrain(circle.x, circle.size / 2 + 30, width - circle.size / 2 - 30);
+    circle.y = constrain(circle.y, circle.size / 2 + 30, height - circle.size / 2 - 105);
+}
+//when mouse moves color of circle changes
+function colorChange() {
+    if (mouseX !== pmouseX || mouseY !== pmouseY) {
+        circle.colorStart.r = random(0, 255);
+        circle.colorStart.g = random(0, 255);
+        circle.colorStart.b = random(0, 255);
+        circle.colorStart.a = random(10, 255);
+    }
+}
+
+//text showing rgba value of circle
+function showText() {
+    fill(0);
+    textSize(20);
+    textAlign(LEFT, CENTER);
+    text('R: ' + int(circle.colorStart.r), width / 5 - 35, 450);//int converts into integer
+    text('G: ' + int(circle.colorStart.g), width / 2 - 86, 450);
+    text('B: ' + int(circle.colorStart.b), width / 2 + 16, 450);
+    text('A: ' + int(circle.colorStart.a), width / 2 + 117, 450);
+}
+
+
+
+/**Game 3 functions */
+
+function game3Setup() {
+    dataInputs3();  //all inputs from data page
+    hideInputs();  //hides all the inputs
+}
+
+function game3Draw() {
+    background(255);
+
+    if (title === true) {
+        background(0);
+        hideInputs();
+        titleScreenText3();
+        enterButton();
+        checkOverlap();
+    }
+
+    else if (data === true) {
+        background(0);
+        showInputs3();
+        dataScreenText();
+        enterButton();
+        checkOverlap();
+    }
+
+    else if (game === true) {
+        background(255);
+        hideInputs3();
+        returnData();
+        //draws all of the circles
+        for (let c of circles) {
+            noStroke();
+            fill(c.color.r, c.color.g, c.color.b, c.color.a);
+            ellipse(c.x, c.y, c.size);
+        }
+    }
+}
+
+
+
+//all title screen text
+function titleScreenText3() {
+    fill(255);
+    textAlign(CENTER, CENTER);
+    textSize(30);
+    text('BUBBLE BUSTER', width / 2, 100);
+
+    //explanation
+    push();
+    textSize(20);
+    text('XXX', width / 2, 150);
+    pop();
+}
+
+
 
 
 //all text on circle data page
@@ -443,7 +646,7 @@ function dataScreenText() {
 }
 
 //all data input boxes
-function dataInputs() {
+function dataInputs3() {
     redMinInput();
     redMaxInput();
     greenMinInput();
@@ -548,11 +751,11 @@ function cNumberInput() {
 
 };
 //hides all data inputs
-function hideInputs() {
+function hideInputs3() {
     for (let i of allInputs) i.hide();
 }
 //shows all data inputs
-function showInputs() {
+function showInputs3() {
     for (let i of allInputs) i.show();
 }
 
