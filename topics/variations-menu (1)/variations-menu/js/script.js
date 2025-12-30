df433876777728ea5443ee6d1ead31d8be877d96
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2025-12-30 04:05:18 -0700

    irrelevant

--- topics/variations-menu (1)/variations-menu/js/script.js
+++ topics/variations-menu (1)/variations-menu/js/script.js
@@ -1,82 +1,60 @@
-/**
- * Variation Menu
- * Pippin Barr
- * 
- * A relatively simple example of a set of variations within a single
- * project. (When we learn Object-Oriented Programming this will be a
- * LOT easier.)
- */
-
 "use strict";
+let speechState = 0;
 
-let state = "menu";
+let speech = {
+    x: 215,
+    y: 240,
+    fill: "#000000",
+    strs: {
+        hey: " Hey! Listen! ",
+        instruction1: "\n   Invasive toxic \n flies are invading \n    the pond ! ...",
+        instruction2: "       Eat flies to save \n    the pond from pollution. \n\n    But don't forget about \n    your health, flies are \n     killing you slowly...",
+        instruction3: "\n  Eat fairies to gain health.\n\n     But doing so pollutes \n       the pond slowly.",
+        size: 40,
+        x: 200,
+        y: 180,
+        delay: 5000,
+    },
+    delay: 2000,
+};
 
-/**
- * Create the canvas
-*/
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(640, 480);
+    angleMode(DEGREES);
+    setTimeout(changeSpeech, speech.delay);
+    setTimeout(changeSpeech2, speech.strs.delay);
+    setTimeout(changeSpeech3, speech.strs.delay + 6000);
 }
 
-
-/**
- * Display the menu or the current variation
-*/
 function draw() {
-    switch (state) {
-        case "menu":
-            menuDraw();
-            break;
-        case "red-variation":
-            redDraw();
-            break
-        case "green-variation":
-            greenDraw();
-            break;
-        case "blue-variation":
-            blueDraw();
-            break;
+    background(255);
+
+    // draw speech
+    fill(speech.fill);
+    textSize(speech.strs.size);
+    textAlign(LEFT, TOP);
+    text(speech.strs.hey, speech.x, speech.y);
+}
+
+function changeSpeech() {
+    speechState = 1;
+    speech.strs.hey = speech.strs.instruction1;
+    speech.strs.size = 30;
+    speech.x = speech.strs.x;
+    speech.y = speech.strs.y;
+}
+
+function changeSpeech2() {
+    if (speechState === 1) {
+        speechState = 2;
+        speech.strs.hey = speech.strs.instruction2;
+        speech.strs.size = 20;
     }
 }
 
-/**
- * Listen for mouse pressed and call the function for it in the
- * current state
- */
-function mousePressed() {
-    switch (state) {
-        case "menu":
-            menuMousePressed();
-            break;
-        case "game1-variation":
-            redMousePressed();
-            break
-        case "game2-variation":
-            greenMousePressed();
-            break;
-        case "game3-variation":
-            blueMousePressed();
-            break;
-    }
-}
-
-/**
- * Listen for keypressed and call the function for it in the
- * current state
- */
-function keyPressed(event) {
-    switch (state) {
-        case "menu":
-            menuKeyPressed(event);
-            break;
-        case "red-variation":
-            redKeyPressed(event);
-            break
-        case "green-variation":
-            greenKeyPressed(event);
-            break;
-        case "blue-variation":
-            blueKeyPressed(event);
-            break;
+function changeSpeech3() {
+    if (speechState === 2) {
+        speechState = 3;
+        speech.strs.hey = speech.strs.instruction3;
     }
 }
