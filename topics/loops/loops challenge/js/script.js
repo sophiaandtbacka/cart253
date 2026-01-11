00b7c56b7b218d5bf4157cac4426ee4b0e30c8dc
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2026-01-11 17:24:15 -0500

    Finished Loop Challenge

--- topics/loops/loops challenge/js/script.js
+++ topics/loops/loops challenge/js/script.js
@@ -1,11 +1,21 @@
 /**
  * Lines
- * Pippin Barr
+ * Sophia and Pippin Barr
  * 
  * A series of lines across the canvas
  */
 
 "use strict";
+
+const NUM_LINES = 501;
+const lines = {
+    x1: 0,
+    x2: 0,
+    y1: 0,
+    y2: 0,
+    stroke: 0,
+    stoke2: 0,
+};
 
 /**
  * Creates the canvas
@@ -21,36 +31,29 @@ function setup() {
 function draw() {
     background("pink");
 
-    stroke(0);
-    line(0, 0, 0, height);
+    lines.stroke = 0;
+    lines.stroke2 = 0;
+    lines.x1 = 0;
+    lines.x2 = 0;
+    lines.y1 = 0;
+    lines.y2 = 0;
 
-    stroke(25);
-    line(50, 0, 50, height);
+    let i = 0;
+    while (i < NUM_LINES) {
+        //vericle lines
+        stroke(lines.stroke);
+        line(lines.x1, 0, lines.x2, height);
+        //horizontal curved lines
+        stroke(lines.stroke2);
+        line(0, lines.y1, width, lines.y2);
 
-    stroke(50);
-    line(100, 0, 100, height);
+        lines.stroke += .8;
+        lines.stroke2 += 10;
+        lines.x1 += 1;
+        lines.x2 += 1;
+        lines.y1 += 1;
+        lines.y2 += 2;
 
-    stroke(75);
-    line(150, 0, 150, height);
-
-    stroke(100);
-    line(200, 0, 200, height);
-
-    stroke(125);
-    line(250, 0, 250, height);
-
-    stroke(150);
-    line(300, 0, 300, height);
-
-    stroke(175);
-    line(350, 0, 350, height);
-
-    stroke(200);
-    line(400, 0, 400, height);
-
-    stroke(225);
-    line(450, 0, 450, height);
-
-    stroke(250);
-    line(500, 0, 500, height);
+        i++
+    }
 }
